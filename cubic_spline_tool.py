5592963437b627e1efd98ca4652e7c02868edb2a
username375 <YaelMa1@ac.sce.ac.il>
2025-06-10 13:04:22 +0300

    colors

--- cubic_spline_tool.py
+++ cubic_spline_tool.py
@@ -1,5 +1,6 @@
 import cubic_spline
 from math import pi
+from colors import bcolors
 
 x_data = [0, pi/6, pi/4, pi/3, pi/2]
 y_data = [0, 0.5, 0.7071, 0.8660, 1.0]
@@ -8,7 +9,7 @@ x_interpolate = pi/3  # Point for which we want to find the interpolated value
 try:
     # Cubic Spline interpolation
     cubic_spline_result = cubic_spline.cubicSplineInterpolation(x_data, y_data, x_interpolate)
-    print(f"Cubic Spline Interpolation: The interpolated value at x = {x_interpolate} is y = {cubic_spline_result}")
+    print(bcolors.OKBLUE + f"Cubic Spline Interpolation: The interpolated value at x = {x_interpolate} is y = {cubic_spline_result}" + bcolors.ENDC)
 
 except ValueError as e:
     print(f"Error: {e}")
